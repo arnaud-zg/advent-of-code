5b0127d9a309be8f7c1e6ba4897db547272d1966
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2025-12-09 21:19:15 +0100

    feat(2025): add puzzle for day 9

---
+++ apps/2025/day-08/puzzle.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, test } from "bun:test";
+
+import { readInput } from "@advent-of-code/utils";
+
+import { solvePuzzle } from "./puzzle";
+
+describe("2025 - Day 8", () => {
+  describe("Playground", () => {
+    describe("Part 1", () => {
+      test("should solve the puzzle with sample", () => {
+        const inputContent = readInput(2025, 8, "sample.txt");
+
+        expect(solvePuzzle(inputContent)).toBe(40);
+      });
+
+      test.skip("should solve the puzzle with input", () => {
+        const inputContent = readInput(2025, 8);
+
+        expect(solvePuzzle(inputContent)).toBeUndefined();
+      });
+    });
+
+    describe.skip("Part 2", () => {
+      test.skip("should solve the puzzle with sample", () => {
+        const inputContent = readInput(2025, 8, "sample.txt");
+
+        expect(solvePuzzle(inputContent)).toBeUndefined();
+      });
+
+      test.skip("should solve the puzzle with input", () => {
+        const inputContent = readInput(2025, 8);
+
+        expect(solvePuzzle(inputContent)).toBeUndefined();
+      });
+    });
+  });
+});
---
+++ apps/2025/day-08/puzzle.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+import { validatePuzzleInput } from "@advent-of-code/utils";
+
+export const solvePuzzle = (inputContent: string): number => {
+  validatePuzzleInput(inputContent);
+
+  return 0;
+};
