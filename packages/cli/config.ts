08739ca0cec9906a56bf776e2ef5358cb9187216
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2025-12-14 00:24:21 +0100

    chore: set max days for 2025 to 12

--- packages/cli/config.ts
+++ packages/cli/config.ts
@@ -4,7 +4,7 @@ export const yearSchema = z
   .string()
   .regex(/^\d{4}$/, "Year must be a 4-digit number");
 
-export const MAX_DAYS = 25;
+export const MAX_DAYS = 12;
 export const FULL_SCORE = 1.0;
 export const PARTIAL_SCORE = 0.5;
 export const NO_SCORE = 0.0;
